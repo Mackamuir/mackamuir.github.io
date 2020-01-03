9a601d7d97f27ce70ab0484e2a73b6b1ee569b6b
Mackamuir <mackamuir@gmail.com>
2020-01-03 16:44:07 +1000

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -23,71 +23,64 @@
          \/____/                  \/____/                  \/____/                  \|___|   
 I Stole all of this v2
 */
-window.onload = function() {
-  
-  var audio = document.getElementById("audio");
-  
-  console.log(audio)
+window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
 
-  //var files = this.files;
-  //audio.src = URL.createObjectURL(audio);
-  audio.load();
-  audio.play();
-  var context = new AudioContext();
-  audio.crossOrigin = "anonymous";
-  var src = context.createMediaElementSource(audio);
-  var analyser = context.createAnalyser();
+var start = function() {
+    var audio = document.getElementById('audio');
+    var ctx = new AudioContext();
+    var analyser = ctx.createAnalyser();
+    var audioSrc = ctx.createMediaElementSource(audio);
+    // we have to connect the MediaElementSource with the analyser 
+    audioSrc.connect(analyser);
+    analyser.connect(ctx.destination);
+    // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+    // analyser.fftSize = 64;
+    // frequencyBinCount tells you how many values you'll receive from the analyser
+    var frequencyData = new Uint8Array(analyser.frequencyBinCount);
 
-  var canvas = document.getElementById("canvas");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  var ctx = canvas.getContext("2d");
-
-  src.connect(analyser);
-  analyser.connect(context.destination);
-
-  analyser.fftSize = 256;
-
-  var bufferLength = analyser.frequencyBinCount;
-  console.log(bufferLength);
-
-  var dataArray = new Uint8Array(bufferLength);
-
-  var WIDTH = canvas.width;
-  var HEIGHT = canvas.height;
-
-  var barWidth = (WIDTH / bufferLength) * 2.5;
-  var barHeight;
-  var x = 0;
-
-  function renderFrame() {
-    requestAnimationFrame(renderFrame);
-
-    x = 0;
-
-    analyser.getByteFrequencyData(dataArray);
-
-var ctx = c.getContext("2d");
-var img = document.getElementById("WEBBG");
-var pat = ctx.createPattern(img, "no-repeat");
-ctx.rect(0, 0, 150, 100);
-ctx.fillStyle = pat;
-ctx.fill();
-
-    for (var i = 0; i < bufferLength; i++) {
-      barHeight = dataArray[i];
-        
-      var r = 255;
-      var g = 255;
-      var b = 255;
-
-      ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-      ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
-
-      x += barWidth + 1;
+    // we're ready to receive some data!
+    var canvas = document.getElementById('canvas'),
+        cwidth = canvas.width,
+        cheight = canvas.height - 2,
+        meterWidth = 10, //width of the meters in the spectrum
+        gap = 2, //gap between meters
+        capHeight = 2,
+        capStyle = '#fff',
+        meterNum = 800 / (10 + 2), //count of the meters
+        capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+    ctx = canvas.getContext('2d'),
+    gradient = ctx.createLinearGradient(0, 0, 0, 300);
+    gradient.addColorStop(1, '#0f0');
+    gradient.addColorStop(0.5, '#ff0');
+    gradient.addColorStop(0, '#f00');
+    // loop
+    function renderFrame() {
+        var array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(array);
+        var step = Math.round(array.length / meterNum); //sample limited data from the total array
+        ctx.clearRect(0, 0, cwidth, cheight);
+        for (var i = 0; i < meterNum; i++) {
+            var value = array[i * step];
+            if (capYPositionArray.length < Math.round(meterNum)) {
+                capYPositionArray.push(value);
+            };
+            ctx.fillStyle = capStyle;
+            //draw the cap, with transition effect
+            if (value < capYPositionArray[i]) {
+                ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+            } else {
+                ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                capYPositionArray[i] = value;
+            };
+            ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+            ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+        }
+        requestAnimationFrame(renderFrame);
     }
-  }
-
-  audio.play();
-  renderFrame();
+    renderFrame();
+    // audio.play();
 };
+
+audio.onplay = function(){
+    start();
+}
